ad8c44ef69ed5c3cbd35320142fa2f650414e145
German <gervg88@gmail.com>
2025-07-08 11:37:02 +0200

    Animaciones funcionales, fondo, suelo y sombra imp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls, Sky } from "@react-three/drei";
 import { Avatar } from "./avatar";
 import { useControls } from "leva";
 
@@ -7,17 +7,23 @@ export const Experience = () => {
   const { animation } = useControls({
     animation: {
       value: 'Typing',
-      options: ["Typing", "Standing", "Falling", "FallingToRoll"]
+      options: ["Typing", "Falling", "FallingToRoll", "Standing"]
     },
   })
   return (
     <>
       <OrbitControls />
+      <Sky />
+      <Environment preset="sunset" />
       <group position-y={-1}>
+        <ContactShadows opacity={0.42} scale={10} blur={2} far={5} resolution={256} color="#000000" />
         <Avatar animation={animation} />
       </group>
-      <ambientLight intensity={1} />
-
+      {/* Suelo fuera del grupo para que esté visible */}
+      <mesh scale={[5, 5, 5]} rotation-x={-Math.PI * 0.5} position-y={-1.001}>
+        <planeGeometry />
+        <meshStandardMaterial color="green" />
+      </mesh>
     </>
   );
 };
--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -13,7 +13,11 @@ import * as THREE from 'three'
 export function Avatar(props) {
 
   const { animation } = props;
-  const { headFollow, cursorFollow } = useControls({ headFollow: false, cursorFollow: false, });
+  const { headFollow, cursorFollow, wireframe } = useControls({
+    headFollow: false,
+    cursorFollow: false,
+    wireframe: false
+  });
 
   const group = useRef();
   const { scene } = useGLTF('models/685e570735fff2860290f57d.glb');
@@ -21,50 +25,55 @@ export function Avatar(props) {
   const { nodes, materials } = useGraph(clone);
 
   const { animations: typingAnimation } = useFBX('animations/Typing.fbx');
-  const { animations: standingAnimation } = useFBX('animations/Standing.fbx');
   const { animations: fallingAnimation } = useFBX('animations/Falling.fbx');
   const { animations: fallingToRollgAnimation } = useFBX('animations/FallingToRoll.fbx');
+  const { animations: standingAnimation } = useFBX('animations/Standing.fbx');
 
   typingAnimation[0].name = 'Typing';
-  standingAnimation[0].name = 'Standing';
   fallingAnimation[0].name = 'Falling';
   fallingToRollgAnimation[0].name = 'FallingToRoll';
+  standingAnimation[0].name = 'Standing';
 
 
-  const { actions } = useAnimations([typingAnimation[0], standingAnimation[0], fallingAnimation[0], fallingToRollgAnimation[0]], group);
+  const { actions } = useAnimations([
+    typingAnimation[0],
+    fallingAnimation[0],
+    fallingToRollgAnimation[0],
+    standingAnimation[0],
+  ], clone);
 
   useFrame((state) => {
-
     if (headFollow) {
-      group.current.getObjectByName("Head").lookAt(state.camera.position);
+      const head = clone.getObjectByName("Head");
+      if (head) head.lookAt(state.camera.position);
     }
     if (cursorFollow) {
       const target = new THREE.Vector3(state.pointer.x, state.pointer.y, 1);
-      group.current.getObjectByName("Spine2").lookAt(target);
+      const spine = clone.getObjectByName("Spine2");
+      if (spine) spine.lookAt(target);
     }
   });
 
   useEffect(() => {
-    actions[animation]?.reset().fadeIn(0.5).play();
-    return () => {
-      actions[animation].reset().fadeOut(0.5).stop();
+    if (animation && actions && actions[animation]) {
+      actions[animation].reset().fadeIn(0.5).play();
+      return () => {
+        if (actions[animation]) {
+          actions[animation].reset().fadeOut(0.5).stop();
+        }
+      }
     }
-  }, [animation]);
+  }, [animation, actions]);
+
+  useEffect(() => {
+    Object.values(materials).forEach((material) => {
+      material.wireframe = wireframe;
+    });
+  }, [wireframe]);
 
   return (
-
     <group {...props} ref={group} dispose={null}>
-      <group rotation-x={-Math.PI / 2} position={[0, 0, 0]}>
-        <primitive object={nodes.Hips} />
-        <skinnedMesh geometry={nodes.Wolf3D_Body.geometry} material={materials.Wolf3D_Body} skeleton={nodes.Wolf3D_Body.skeleton} />
-        <skinnedMesh geometry={nodes.Wolf3D_Outfit_Bottom.geometry} material={materials.Wolf3D_Outfit_Bottom} skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton} />
-        <skinnedMesh geometry={nodes.Wolf3D_Outfit_Footwear.geometry} material={materials.Wolf3D_Outfit_Footwear} skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton} />
-        <skinnedMesh geometry={nodes.Wolf3D_Outfit_Top.geometry} material={materials.Wolf3D_Outfit_Top} skeleton={nodes.Wolf3D_Outfit_Top.skeleton} />
-        <skinnedMesh name="EyeLeft" geometry={nodes.EyeLeft.geometry} material={materials.Wolf3D_Eye} skeleton={nodes.EyeLeft.skeleton} morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary} morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences} />
-        <skinnedMesh name="EyeRight" geometry={nodes.EyeRight.geometry} material={materials.Wolf3D_Eye} skeleton={nodes.EyeRight.skeleton} morphTargetDictionary={nodes.EyeRight.morphTargetDictionary} morphTargetInfluences={nodes.EyeRight.morphTargetInfluences} />
-        <skinnedMesh name="Wolf3D_Head" geometry={nodes.Wolf3D_Head.geometry} material={materials.Wolf3D_Skin} skeleton={nodes.Wolf3D_Head.skeleton} morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary} morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences} />
-        <skinnedMesh name="Wolf3D_Teeth" geometry={nodes.Wolf3D_Teeth.geometry} material={materials.Wolf3D_Teeth} skeleton={nodes.Wolf3D_Teeth.skeleton} morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary} morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences} />
-      </group>
+      <primitive object={clone} />
     </group>
   )
 }
